1a4e379cd72e963a91910378ed6c786c7f5153a8
johnalexanderg <johnalexand3rg@gmail.com>
2024-06-25 20:36:21 -0500

    tarjeta de credito - john

---
+++ johnalexanderg/tarjeta_de_credito.js
@@ -0,0 +1,9 @@
+let numeroDeCuenta = document.getElementById("numeroDeCuenta"); //  == $0
+let numeroValidacion // conde here...
+let nombreTitular // conde here...
+
+console.log(numeroDeCuenta.textContent)
+
+numeroDeCuenta.textContent = prompt("Ingrese el numero de cuenta");
+numeroValidacion.textContent = prompt();
+nombreTitular.textContent = prompt();
