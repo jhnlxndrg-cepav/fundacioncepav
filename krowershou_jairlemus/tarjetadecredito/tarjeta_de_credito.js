0a4bff5ad046aaa1b562ca2fb7bf127d83a40d15
Jair Lemus <lemusortiznew@gmail.com>
2024-06-30 22:36:33 -0500

    tarjeta con formulario

--- krowershou_jairlemus/tarjetadecredito/tarjeta_de_credito.js
+++ krowershou_jairlemus/tarjetadecredito/tarjeta_de_credito.js
@@ -3,6 +3,8 @@ let fechaValidacion =document.getElementById("fechaValidacion"); //  == $0
 let titularTarjeta =document.getElementById("titularTarjeta"); //  == $0
 
 console.log(numeroDeCuenta.textContent)
+console.log(fechaValidacion.textContent)
+console.log(titularTarjeta.textContent)
 
 
 numeroDeCuenta.textContent = prompt("Ingrese el numero de cuenta");
