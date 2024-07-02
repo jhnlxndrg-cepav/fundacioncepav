296e8c9c8f89cfcce86ce7da6f17036d139408c8
Jair Lemus <lemusortiznew@gmail.com>
2024-07-02 12:11:56 -0500

    parte de java y formulario y flip card pendiente

--- krowershou_jairlemus/tarjetadecredito/tarjeta_de_credito.js
+++ krowershou_jairlemus/tarjetadecredito/tarjeta_de_credito.js
@@ -2,11 +2,36 @@ let numeroDeCuenta = document.getElementById("numeroDeCuenta"); //  == $0
 let fechaValidacion =document.getElementById("fechaValidacion"); //  == $0 
 let titularTarjeta =document.getElementById("titularTarjeta"); //  == $0
 
-console.log(numeroDeCuenta.textContent)
-console.log(fechaValidacion.textContent)
-console.log(titularTarjeta.textContent)
+
+// console.log(numeroDeCuenta.textContent)
+// console.log(fechaValidacion.textContent)
+// console.log(titularTarjeta.textContent)
 
 
-numeroDeCuenta.textContent = prompt("Ingrese el numero de cuenta");
-fechaValidacion.textContent = prompt("Ingrese efecha de vencimiento");
-titularTarjeta.textContent = prompt("Ingrese nombre del titular");
+// numeroDeCuenta.textContent = prompt("Ingrese el numero de cuenta");
+// fechaValidacion.textContent = prompt("Ingrese efecha de vencimiento");
+// titularTarjeta.textContent = prompt("Ingrese nombre del titular");
+
+let inputNumeroDeCuenta = document.getElementById("numeroDeTarjeta");
+inputNumeroDeCuenta.addEventListener("keyup" , function name(evento) {
+    numeroDeCuenta.textContent = evento.target.value
+    
+})
+
+
+let inputFechaValidacion = document.getElementById("fechaVencimiento");
+inputFechaValidacion.addEventListener("keyup" , function name(evento) {
+    fechaValidacion.textContent = evento.target.value
+
+})
+
+
+let inputTitularTarjeta = document.getElementById("dueñoTarjeta");
+inputTitularTarjeta.addEventListener("keyup" , function name(evento) {
+    titularTarjeta.textContent = evento.target.value
+})
+
+let inputCcv = document.getElementById("ccv");
+inputCcv.addEventListener("keyup" , function name(evento) {
+    ccv.textContent = evento.target.value
+})
